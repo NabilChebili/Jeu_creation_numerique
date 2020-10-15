0746c1249c996f651072184414400dc430542e11
Thomas <thomas.rabian@gmail.com>
2020-10-15 10:22:01 +0200

    Enlever contenu code

--- Code/Dog.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-
-
-
-func _on_Dog_body_entered(body : Area2D)->void:
-	pass # Replace with function body.
--- Code/shozo.gd
+++
@@ -1,8 +0,0 @@
-extends StaticBody2D
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-func _ready():
-	$AnimatedSprite.play("default")
